adf386c0f0054b5be1196868bd70ea385dfde496
marianowadi <marianojacobo@gmail.com>
2024-02-23 15:59:00 -0300

    feat: first commit!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
+type CountryValues = {
+    name: string
+    checked: boolean
+}
+const COUNTRIES = ['India', 'USA', 'France']
+const COUNTRIES_DATA: CountryValues[] = COUNTRIES.map((country) => ({
+    name: country,
+    checked: false,
+}))
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    const [selectedCountries, setSelectedCountries] =
+        useState<CountryValues[]>(COUNTRIES_DATA)
+
+    const handleCheckboxChange = (country: CountryValues, index: number) => {
+        const values = [...selectedCountries]
+        values.splice(index, 1, {
+            ...country,
+            checked: !country.checked,
+        })
+        setSelectedCountries(values)
+    }
+
+    const allCountriesSelected = selectedCountries.every(
+        (country) => country.checked
+    )
+
+    const handleSelectAllChange = () => {
+        setSelectedCountries((prev) =>
+            prev.map((country) => ({
+                ...country,
+                checked: allCountriesSelected ? false : true,
+            }))
+        )
+    }
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                placeContent: 'center',
+                margin: 'auto auto',
+                width: '20vw',
+                height: '80vh',
+                fontSize: '1.5rem',
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <div>
+                <input
+                    type="checkbox"
+                    id={`select_all_id`}
+                    checked={allCountriesSelected}
+                    onChange={() => handleSelectAllChange()}
+                />
+                <label htmlFor={`select_all_id`}>Select all</label>
+            </div>
+            {selectedCountries.map((country, i) => (
+                <div key={country.name}>
+                    <input
+                        type="checkbox"
+                        id={`${country.name}_id`}
+                        checked={country.checked}
+                        onChange={() => handleCheckboxChange(country, i)}
+                    />
+                    <label htmlFor={`${country.name}_id`}>{country.name}</label>
+                </div>
+            ))}
+        </div>
+    )
 }
 
-export default App;
+export default App
